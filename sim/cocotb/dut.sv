8df0008c8a6c77f5a651a4d96313259844fbadd4
Mike Kuskov <m.kuskov@innopolis.university>
2025-02-11 01:31:01 +0300

    Update cocotb testbench

--- sim/cocotb/dut.sv
+++ sim/cocotb/dut.sv
@@ -17,52 +17,52 @@ module dut
     output                           down_valid,
     output              [XLEN - 1:0] down_data
 );
-	
+
     initial $dumpvars;
 
-	logic in_valid, out_valid;
-	logic [WIDTH - 1:0][XLEN - 1:0] in_data, out_data;
+    logic in_valid, out_valid;
+    logic [WIDTH - 1:0][XLEN - 1:0] in_data, out_data;
 
     serial_to_parallel #(
-		.XLEN  ( XLEN  ),
-		.WIDTH ( WIDTH )
-	) i_stp (
-		.clk ( clk ),
-		.rst ( rst ),
+        .XLEN  ( XLEN  ),
+        .WIDTH ( WIDTH )
+    ) i_stp (
+        .clk ( clk ),
+        .rst ( rst ),
 
-		.serial_valid   ( up_valid ),
-		.serial_data    ( up_data  ),
+        .serial_valid   ( up_valid ),
+        .serial_data    ( up_data  ),
 
-		.parallel_valid ( in_valid ),
-		.parallel_data  ( in_data  )
+        .parallel_valid ( in_valid ),
+        .parallel_data  ( in_data  )
     );
 
-	circular_convolution #( .QLEN(XLEN), .WINDOW_SIZE(WIDTH) ) i_cc
-	(
-		.clk ( clk ),
-    	.rst ( rst ),
+    circular_convolution #( .QLEN(XLEN), .WINDOW_SIZE(WIDTH) ) i_cc
+    (
+        .clk ( clk ),
+        .rst ( rst ),
 
-		.weights  ( weights ),
+        .weights  ( weights ),
 
-		.in_valid ( in_valid ),
-    	.in_data  ( in_data  ),
+        .in_valid ( in_valid ),
+        .in_data  ( in_data  ),
     
-		.out_valid ( out_valid ),
-    	.out_data  ( out_data  )
-	);
-	
+        .out_valid ( out_valid ),
+        .out_data  ( out_data  )
+    );
+    
     parallel_to_serial #(
-		.XLEN  ( XLEN  ),
-		.WIDTH ( WIDTH )
-	) i_pts (
-		.clk ( clk ),
-		.rst ( rst ),
+        .XLEN  ( XLEN  ),
+        .WIDTH ( WIDTH )
+    ) i_pts (
+        .clk ( clk ),
+        .rst ( rst ),
 
-		.parallel_valid ( out_valid ),
-		.parallel_data  ( out_data  ),
+        .parallel_valid ( out_valid ),
+        .parallel_data  ( out_data  ),
 
-		.serial_valid   ( down_valid ),
-		.serial_data    ( down_data  )
+        .serial_valid   ( down_valid ),
+        .serial_data    ( down_data  )
     );
 
 endmodule
--- sim/cocotb/test.py
+++ sim/cocotb/test.py
@@ -1,29 +1,75 @@
 import time
 import cocotb
-from cocotb.triggers import Timer, RisingEdge
 from cocotb.clock import Clock
+from cocotb.handle import SimHandleBase
+from cocotb.queue import Queue
+from cocotb.triggers import RisingEdge
+from cocotb.types import LogicArray, Range
+import numpy as np
+
 
 if not cocotb.simulator.is_running():
     raise RuntimeError("Running Cocotb testbench without the simulator")
 
-DATA_LEN   = int(cocotb.top.XLEN.value)
+XLEN   = int(cocotb.top.XLEN.value)
 DATA_WIDTH = int(cocotb.top.WIDTH.value)
+
+# dump_vars?
+# reset
+# init
+# jork {
+# driver - drive - input up_a, up_b, down_ready
+# monitor - collect data from inputs & outputs into queues or mailboxes
+# scoreboard - recieve data from queues or mailboxes and verify it against golden model
+# } join
+
+
+async def drive_input(dut: SimHandleBase, ):
+    pass
+
+
+async def monitor_valid(clk: SimHandleBase, valid: SimHandleBase, data: SimHandleBase, queue: Queue[LogicArray]):
+    while True:
+        await RisingEdge(clk)
+        if valid.value != "1":
+            await RisingEdge(valid)
+            continue
+        queue.put_nowait(data.value)
+
+
+async def scoreboard(queue_in: Queue[LogicArray], queue_out: Queue[LogicArray]):
+    pass
+
+
+async def reset(clk: SimHandleBase, rst: SimHandleBase):
+    rst.value = 1
+    for _ in range(3):
+        await RisingEdge(clk)
+    rst.value = 0
+    await RisingEdge(clk)
 
 
 @cocotb.test()
 async def my_first_test(dut):
-    """Try accessing the design."""
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start()) 
-
     dut._log.info("Initialize and reset model")
 
-    dut.rst.value = 1
-    for _ in range(3):
-        await RisingEdge(dut.clk)
-    dut.rst.value = 0
-    await RisingEdge(dut.clk)
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start()) 
+    await reset()
 
-    dut._log.info(f'DATA LEN & WIDTH: {DATA_LEN}, {DATA_WIDTH}')
+    in_queue = Queue()
+    out_queue = Queue()
+
+    drive_in = cocotb.start_soon(drive_input(dut.clk, dut.in_valid, dut.in_data))
+
+    monitor_in = cocotb.start_soon(monitor_valid(dut.clk, dut.in_valid, dut.in_data, in_queue))
+    monitor_out = cocotb.start_soon(monitor_valid(dut.clk, dut.out_valid, dut.out_data, out_queue))
+
+    scoreboard = cocotb.start_soon(scoreboard())
+
+    dut._log.info(f'DATA LEN & WIDTH: {XLEN}, {DATA_WIDTH}')
+
+    kernel = np.concat([np.zeros((DATA_WIDTH//4, )), np.ones((DATA_WIDTH//2,)), np.zeros((DATA_WIDTH//4,))])
+    # kernel = np.random.normal(scale=1, size=DATA_WIDTH)
 
     for _ in range(1_000):
         await RisingEdge(dut.clk)
