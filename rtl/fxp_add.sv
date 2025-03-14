fef4b42add7fd309b332081fb015d10a8c5b6a51
CaneRig <originalomail@gmail.com>
2025-03-14 11:18:25 +0300

    IVerilog compatibility fix

--- rtl/fxp_add.sv
+++ rtl/fxp_add.sv
@@ -1,8 +1,7 @@
-module fxp_add
-# (
-    parameter int QLEN = 16,
-              int FRAC_SIZE = 12,
-              int INT_SIZE  = QLEN - FRAC_SIZE
+module fxp_add# (
+     parameter  QLEN = 16,
+                FRAC_SIZE = 12,
+                INT_SIZE  = QLEN - FRAC_SIZE
 )
 (
     input  signed [QLEN-1:0] a,
--- rtl/fxp_mult.sv
+++ rtl/fxp_mult.sv
@@ -1,8 +1,8 @@
 module fxp_mult
 # (
-    parameter int QLEN = 16,
-              int FRAC_SIZE = 12,
-              int INT_SIZE  = QLEN - FRAC_SIZE
+    parameter QLEN = 16,
+              FRAC_SIZE = 12,
+              INT_SIZE  = QLEN - FRAC_SIZE
 )
 (
     input  signed [QLEN-1:0] a,
